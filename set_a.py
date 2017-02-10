0c40dc2d8d375786114d1a0e77bb82cdee3d5bbc
MasonAWing <wingma071700@cvh.rusd.us>
2017-02-10 09:45:32 -0800

    3.4

--- set_a.py
+++ set_a.py
@@ -19,3 +19,8 @@ print(fullname[0:6])
 3.3 Using the slice operator, print your last name.
 """
 print(fullname[6:11])
+"""
+3.4 Using the slice and concatenation operators, print your name in the
+form "Lastname, Firstname."
+"""
+print(fullname[6:11] + fullname[0:6])
