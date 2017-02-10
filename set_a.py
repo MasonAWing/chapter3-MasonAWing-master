ec65799a458bb97f13cbdde7045d9a89f08b1eca
MasonAWing <wingma071700@cvh.rusd.us>
2017-02-10 09:43:53 -0800

    3.1

---
+++ set_a.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# p. 89
+#
+
+"""
+3.1 Create a string variable that is initialized to your entire name -
+first, middle, and last.
+"""
+fname = 'Mason '
+lname = 'Wing'
+fullname = fname + lname
+print(fullname)
