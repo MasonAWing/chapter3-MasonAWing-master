6da8d5363690e831725060f322e03e2055863803
MasonAWing <wingma071700@cvh.rusd.us>
2017-02-10 09:48:31 -0800

    3.8

---
+++ set_b.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#
+# pp. 91, 93
+#
+
+"""
+3.8 Using the count method, find the number of occurrences of the
+character 's' in the string 'mississippi'.
+"""
+str = 'mississippi'
+sub = 's' ;
+print(str.count(sub, 0, 12))
